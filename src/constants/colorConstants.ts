08310d6915539297b724dcb5b973a971a19ed9a2
mhazizk <mhazizk@gmail.com>
2023-05-02 20:33:06 +0700

    Create colorConstants.ts

---
+++ src/constants/colorConstants.ts
@@ -0,0 +1,9 @@
+const colorConstants = {
+  primary: "#C956FF",
+  secondary: "#8120AE",
+  text: "#ffffff",
+  screenBackground: "#1A1A1A",
+  cardBackground: "#892CFF",
+};
+
+export default colorConstants;
