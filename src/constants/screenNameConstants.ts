8088e785f99dda868cf1c7740846ef6f19abea0b
mhazizk <mhazizk@gmail.com>
2023-05-02 20:33:08 +0700

    Create screenNameConstants.ts

---
+++ src/constants/screenNameConstants.ts
@@ -0,0 +1,16 @@
+const screenNameConstants = {
+  dashboardScreen: {
+    id: "dashboardScreen",
+    title: "Dashboard",
+  },
+  exploreScreen: {
+    id: "exploreScreen",
+    title: "Explore",
+  },
+  profileScreen: {
+    id: "profileScreen",
+    title: "Profile",
+  },
+};
+
+export default screenNameConstants;
