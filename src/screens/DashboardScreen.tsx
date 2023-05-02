066cafb431f5ea96ff8fac23dbe04e258203661e
mhazizk <mhazizk@gmail.com>
2023-05-02 20:33:13 +0700

    Create DashboardScreen.tsx

---
+++ src/screens/DashboardScreen.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+type DashboardScreenProps = {
+  navigation: any;
+};
+
+const DashboardScreen = () => {
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  );
+};
+
+export default DashboardScreen;
