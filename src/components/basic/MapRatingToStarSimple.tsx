7d26ad6526e1baef5e37ecd6e9e1137e48ba37f0
mhazizk <mhazizk@gmail.com>
2023-05-03 22:13:45 +0700

    Create MapRatingToStarSimple.tsx

---
+++ src/components/basic/MapRatingToStarSimple.tsx
@@ -0,0 +1,36 @@
+import { View, Text } from "react-native";
+import React from "react";
+import IonIcons from "react-native-vector-icons/Ionicons";
+
+interface MapRatingToStarProps {
+  rating: number;
+}
+
+const MapRatingToStarSimple: React.FC<MapRatingToStarProps> = ({ rating }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "baseline",
+        justifyContent: "center",
+        backgroundColor: "rgba(0,0,0,0.5)",
+        paddingHorizontal: 8,
+        paddingVertical: 4,
+        borderTopRightRadius: 8,
+      }}
+    >
+      <IonIcons name="star" size={16} color="yellow" />
+      <Text
+        style={{
+          paddingLeft: 4,
+          color: "white",
+          fontSize: 16,
+        }}
+      >
+        {rating}
+      </Text>
+    </View>
+  );
+};
+
+export default MapRatingToStarSimple;
