8b890699a64373af39bce14c9f371ca2272b8044
mhazizk <mhazizk@gmail.com>
2023-05-03 22:13:38 +0700

    Create LocationDetailsScreen.tsx

---
+++ src/screens/LocationDetailsScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const LocationDetailsScreen = () => {
+  return (
+    <View>
+      <Text>LocationDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default LocationDetailsScreen;
