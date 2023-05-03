7448e9d852e7a4d527cc8132749145b39c01ed7c
mhazizk <mhazizk@gmail.com>
2023-05-03 22:13:54 +0700

    Update MyProfileScreen.tsx

--- src/screens/MyProfileScreen.tsx
+++ src/screens/MyProfileScreen.tsx
@@ -1,10 +1,38 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import WorkspotType from "../types/workspotType";
+import WorkspotsImageGrid from "../components/section/ImageGrid";
+import { Image } from "expo-image";
+import LocationCoverImage from "../components/basic/LocationCoverImage";
+
+const dummyData = (): WorkspotType[] => {
+  const data: WorkspotType[] = [];
+  let counter = 0;
+  while (counter < 10) {
+    data.push({
+      id: String(counter),
+      name: `Workspot ${counter}`,
+      description: `Description ${counter}`,
+      location: `Location ${counter}`,
+      imageURI: `https://picsum.photos/200/300?random=${counter}`,
+      rating: Math.random() * 5,
+      price: Math.random() * 100,
+    });
+    counter++;
+  }
+  return data;
+};
 
 const MyProfileScreen = () => {
+  const [workspots, setWorkspots] = useState<WorkspotType[]>(() => dummyData());
+
   return (
     <View>
-      <Text>MyProfileScreen</Text>
+      <LocationCoverImage
+        imageURI="https://picsum.photos/200/300?random=1"
+        rating={4}
+      />
+      <WorkspotsImageGrid workspots={workspots} />
     </View>
   );
 };
