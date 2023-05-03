cdab0a4c529433303a2fa0724afccf96a3b4bbe5
mhazizk <mhazizk@gmail.com>
2023-05-03 22:13:35 +0700

    Create workspotType.ts

---
+++ src/types/workspotType.ts
@@ -0,0 +1,11 @@
+type WorkspotType = {
+  name: string;
+  imageURI: string;
+  rating: number;
+  location: string;
+  description: string;
+  price: number;
+  id: string;
+};
+
+export default WorkspotType;
