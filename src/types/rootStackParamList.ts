16391b6264f3b0a34b6d2d66728056276124c764
mhazizk <mhazizk@gmail.com>
2023-05-04 22:19:06 +0700

    Create rootStackParamList.ts

---
+++ src/types/rootStackParamList.ts
@@ -0,0 +1,9 @@
+import WorkspotDetailsType from "./workspotDetailsType";
+
+type RootStackParamList = {
+  BottomTab: undefined;
+  Profile: undefined;
+  WorkspotDetails: { workspot: WorkspotDetailsType };
+};
+
+export default RootStackParamList;
