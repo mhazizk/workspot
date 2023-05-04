5832fd09885edb8089b365fcc78c0990b067c08e
mhazizk <mhazizk@gmail.com>
2023-05-04 22:18:57 +0700

    Update StackNavigation.tsx

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -1,11 +1,28 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigation from "./TabNavigation";
+import WorkspotDetailsScreen from "../screens/WorkspotDetailsScreen";
+import RootStackParamList from "../types/rootStackParamList";
 
+const Stack = createStackNavigator<RootStackParamList>();
 const StackNavigation = () => {
   return (
-    <View>
-      <Text>StackNavigation</Text>
-    </View>
+    <>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="BottomTab"
+          component={TabNavigation}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="WorkspotDetails"
+          component={WorkspotDetailsScreen}
+        />
+      </Stack.Navigator>
+    </>
   );
 };
 
