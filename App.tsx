ff110b573ec6822fd4bc32c378b68d913dd10627
mhazizk <mhazizk@gmail.com>
2023-05-04 22:18:28 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,13 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import TabNavigation from "./src/navigation/TabNavigation";
 import { NavigationContainer, DarkTheme } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import StackNavigation from "./src/navigation/StackNavigation";
 
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
       <NavigationContainer theme={DarkTheme}>
-        <TabNavigation />
+        <StackNavigation />
       </NavigationContainer>
     </>
   );
